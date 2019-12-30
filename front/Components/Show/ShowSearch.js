7b12840e18a82e5167481ad313bfc45a2e11a0ae
JoaoCoelho8 <33787@ufp.edu.pt>
2019-12-30 20:50:26 +0000

    New ways to search for recipes

--- front/Components/Show/ShowSearch.js
+++ front/Components/Show/ShowSearch.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Container, Content, Header, Field, Left, Body, Right, Title, Form, Item, Input, Icon, Label, Button, Text, TextInput } from 'native-base';
+import { Container, Content, Header, Field, Left, Body, Right, Title, Form, Item, Input, Label, Button, Text, TextInput } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import axios from 'axios';
 import ShowContent from './ShowContent'
 import { Keyboard } from 'react-native'
+
+import { Dropdown } from 'react-native-material-dropdown';
+
+import { Platform, StyleSheet, View, Alert, YellowBox} from "react-native";
+import { Menu, MenuProvider, MenuOptions, MenuOption, MenuTrigger} from "react-native-popup-menu";
 
 
 
@@ -11,12 +17,24 @@ function ShowSearch(props) {
     const [search, setSearch] = useState("");
     const [recipes, setRecipes] = useState([]);
     const [isError, setIsError] = useState(false);
+    const [searchByMeal, setSearchByMeal] = useState(false);
+    const [data, setData] = useState([{
+        value: 'Breakfast & Brunch',
+      }, {
+        value: 'Lunch/Dinner',
+      }, {
+        value: 'Desert',
+      }, {
+        value: 'Appetizers & Snacks',
+      },{
+        value: 'Drinks',
+      },]);
+      const [meal, setMeal] = useState("");
 
     useEffect(() => {
         //axios.get("http://192.168.1.68:8000/api/searchRecipeAll)
         axios.get("http://192.168.1.119:8000/api/searchRecipeAll")
         .then(result => {
-            console.log(result.data)
             if (result.status==200) { 
                 setRecipes(result.data);
             } else {
@@ -31,7 +49,6 @@ function ShowSearch(props) {
         //axios.get("http://192.168.1.68:8000/api/searchRecipeName/name/"+search.text)
         axios.get("http://192.168.1.119:8000/api/searchRecipeName/name/"+search.text)
         .then(result => {
-            console.log(result.data)
             if (result.status==200) { 
                 setRecipes(result.data);
             } else {
@@ -42,12 +59,29 @@ function ShowSearch(props) {
         });
     };
 
+    const menuPress = () => {
+        console.log(meal);
+        if(searchByMeal==false){
+            setSearchByMeal(true)
+        } else {
+            setSearchByMeal(false)
+            //setMeal("");
+        }
+    }
+    
     return (
         <Content padder>
             <Item>
-                <Icon name="ios-menu" /*menu???*//>
+                <Icon name="bars" style={{fontSize: 30}} onPress={menuPress} />
                 <Input placeholder="Search" onChangeText={(text) => setSearch({text})} returnKeyType={"search"} onSubmitEditing={()=>{ addEntryClick(); Keyboard.dismiss();}} blurOnSubmit={true} />
             </Item>
+            { searchByMeal &&
+                <Dropdown
+                label='Search by type of meal'
+                data={data}
+                onChangeText={(value) => setMeal({value})}
+                />
+            }
             <ShowContent recipes={recipes} />
         </Content>
     );
